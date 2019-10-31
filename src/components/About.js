c6993b0ee2c1f11a8f4e8fb4c14dd9fea0d7c7c5
Publikwerker <hello@publikwerks.com>
2019-10-31 09:58:49 -0700

    moved email form from about to footer.

--- src/components/About.js
+++ src/components/About.js
@@ -10,21 +10,6 @@ export default function About() {
       <div className="row">
         <p className="paragraph">Walter offers more than paltry banter and potty humor. Walter is comedian and minstrel, educator and mystic. His performance transcends modern comedy's vaudevillian origins, breaking the viewer/spectacle barrier, evoking in the audience a crisis of identity and introspection.</p>
       </div>
-      <div className="email-form__container">
-      <h3 className="heading-tertiary">Walter can help you, too!</h3>
-        <form className="email-form">
-          <label className="formEmail input-label" for="email">
-            <input className="email-form__input" name="email" type="email" required="true" />
-            email</label>
-          <label className="formName input-label" for="formName">
-            <input className="email-form__input" name="formName" type="text" />
-            name</label> 
-          <label className="input-label" for="message">
-            <textarea className="formMessage email-form__input" name="message" />
-            Message</label>
-          <input classname="submit-button email-form__input" type="submit" value="Submit" />
-        </form>
-      </div>
     </section>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,30 @@ import bgVideomp4 from  "../images/walter-talent-show.mp4";
 export default function Footer() {
   return (
     <footer className="footer">
-    <div className="bg-video">
-      <video className="bg-video__content" autoPlay muted loop>
-        <source src={bgVideomp4} type="video/mp4">
-        </source>
-        {/* <source src={bgVideowebm} type="video/webm">
-        </source> */}
-        Your Browser is not Supported!
-      </video>
-    </div>  
+      <div className="bg-video">
+        <video className="bg-video__content" autoPlay muted loop>
+          <source src={bgVideomp4} type="video/mp4">
+          </source>
+          {/* <source src={bgVideowebm} type="video/webm">
+          </source> */}
+          Your Browser is not Supported!
+        </video>
+      </div>  
+      <div className="email-form__container">
+      <h3 className="heading-tertiary">Walter can help you, too!</h3>
+        <form className="email-form">
+          <label className="formEmail input-label" for="email">
+            <input className="email-form__input" name="email" type="email" required="true" />
+            email</label>
+          <label className="formName input-label" for="formName">
+            <input className="email-form__input" name="formName" type="text" />
+            name</label> 
+          <label className="input-label" for="message">
+            <textarea className="formMessage email-form__input" name="message" />
+            Message</label>
+          <input classname="submit-button email-form__input" type="submit" value="Submit" />
+        </form>
+      </div>
       <p className="footer--text">All content Copyright &copy; 2019 Publikwerks, LLC</p>
     </footer>
   )
