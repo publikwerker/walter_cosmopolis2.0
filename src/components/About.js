e7c2ffce8687199cca6bc8c6a155bc17ba50ad5a
Publikwerker <hello@publikwerks.com>
2019-11-21 17:21:07 -0800

    homogenized css between about, performance, and outreach sections.

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
       <h2 className=" heading-secondary about__title">Proven Crowd Pleaser</h2>
         <h3 className="heading-tertiary">Walter Cosmopolis is a world-class entertainer.</h3>
     </div>
-    <div className="row">
+    <div className="row with-form">
       <p className="paragraph">Walter offers more than paltry banter and potty humor. Walter is comedian and minstrel, educator and mystic. With insightful eloquence, Walter speaks personally on topics ranging from Race to Religion, from Gender to Sex, and from Gentrification to Homelessness. Walter's performance transcends modern comedy's vaudevillian origins, breaking the viewer/spectacle barrier, evoking in the audience a crisis of identity and introspection.</p>
 
       <form action="mailto:hello@waltercosmopolis.com" method="get" enctype="text/plain">
