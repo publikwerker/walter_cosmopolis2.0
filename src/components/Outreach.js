77073074e31a29d83d373dea3f0fda5d8b1371a4
Publikwerker <hello@publikwerks.com>
2019-11-28 13:02:01 -0800

    finished outreach section

--- src/components/Outreach.js
+++ src/components/Outreach.js
@@ -17,14 +17,14 @@ export default function Outreach() {
                 &nbsp;
               </div>
               <h4 className="card__heading">
-                <span className="card__heading-span card__heading-span--1">Walter Has Fought Demons!</span>
+                <span className="card__heading-span card__heading-span--1">Laser-like focus</span>
               </h4>
             </div>
 
             <div className="card__side card__side--back  card__side--back-1">
               <div className="card__desc">
-              <p className="paragraph">Addiction comes in many forms. Walter has faced these demons, figuratively and literally. "The key is to have faith in yourself."</p>
-                <a href="#" className="btn btn--white">Learn more!</a>
+                <h3 className='heading-tertiary'>Focus Your Intention</h3>
+                <p className="paragraph">Set your sights upon your goals, and don't let anything distract you until you reach them. The stronger your focus, the faster your new reality manifests. Seek those who encourage you, and avoid complainers.</p>
               </div>
             </div>
           </div>
@@ -37,53 +37,35 @@ export default function Outreach() {
                 &nbsp;
               </div>
               <h4 className="card__heading">
-                <span className="card__heading-span card__heading-span--2">Walter Fights for Justice!</span>
+                <span className="card__heading-span card__heading-span--2">Perseverence and Tenacity</span>
               </h4>
-          
             </div>
 
             <div className="card__side card__side--back  card__side--back-2">
               <div className="card__desc">
-              <p className="paragraph">In his youth, Walter saw injustice in the world. Realizing that he was the one he was waiting for, young Walter put on his boots and went on a Mission.</p>
-                <a href="#" className="btn btn--white">Learn more!</a>
+                <h3 className='heading-tertiary'>Failure Is Part of Success</h3>
+                <p className="paragraph">At any given moment of your life, your very best is all you can do, so do it! If you fail, you will know you did your best. Pick yourself up, and keep getting back up. Never take no for an answer! You deserve to live the life you love.</p>
               </div>
             </div>
           </div>
-            <div className="card__side card__side--front">
-              <div className="card__picture card__picture--1">
-                &nbsp;
-              </div>
-              <h4 className="card__heading">
-                <span className="card__heading-span card__heading-span--1">Walter Fights for Truth and Justice</span>
-              </h4>
-          
-            </div>
-
-            <div className="card__side card__side--back  card__side--back-2">
-              <div className="card__desc">
-              <p className="paragraph">In his youth, Walter saw injustice in the world. Realizing that he was the one he was waiting for, young Walter put on his boots and went on a Mission.</p>
-                <a href="#" className="btn btn--white">Learn more!</a>
-              </div>
-            </div>
-          </div>
+        </div>
 
         <div className="card-box">
-
           <div className="card">
             <div className="card__side card__side--front">
               <div className="card__picture card__picture--3">
                 &nbsp;
               </div>
               <h4 className="card__heading">
-                <span className="card__heading-span card__heading-span--3">Walter Always Wins!</span>
+                <span className="card__heading-span card__heading-span--3">Celebrate every victory</span>
               </h4>
           
             </div>
 
             <div className="card__side card__side--back  card__side--back-3">
               <div className="card__desc">
-              <p className="paragraph">In his youth, Walter saw injustice in the world. Realizing that he was the one he was waiting for, young Walter put on his boots and went on a Mission.</p>
-                <a href="#" className="btn btn--white">Learn more!</a>
+                <h3 className='heading-tertiary'>Give Thanks Constantly</h3>
+                <p className="paragraph">Imagine yourself where you want to be, and thank the Universe for putting you there. Celebrate every step along the journey for bringing you closer to your dream reality. Know that is your future, and give thanks!</p>
               </div>
             </div>
           </div>
