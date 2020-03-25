70517e5dce56bc5a8fbf098c5e293c7d7899ec85
Publikwerker <hello@publikwerks.com>
2020-03-25 15:59:05 -0700

    Added new talentLink element on the Perfomance component.

--- src/components/Performance.js
+++ src/components/Performance.js
@@ -3,10 +3,12 @@ import React from 'react';
 import tenSeconds from "../images/wc_ten_seconds.png";
 import motivational from "../images/wc_motivational_title.png";
 import unboxing from "../images/wc_unboxing_title.png";
+import breadAndCircus from "../images/wc_bread_and_circus.png";
 
 const talentLink1 = "https://www.youtube.com/playlist?list=PLtxzOWaznJ8dZpvahAM7FBY_f4y9CmNVy";
 const talentLink2 = "https://www.youtube.com/playlist?list=PLtxzOWaznJ8fi0ay4RCgsx9FcLWEQp2jn";
 const talentLink3 ="https://www.youtube.com/playlist?list=PLtxzOWaznJ8cmbfwVaTtzmYSvSI7Yvl2s";
+const talentLink4 = "https://www.youtube.com/playlist?list=PLtxzOWaznJ8dkPvjD7DLWguLKbucW9411";
 
 export default function Performance () {
 
@@ -53,6 +55,19 @@ export default function Performance () {
                   className="talent__image"
                   alt="Walter in midair, inverted, seemingly floating over the grass beneath a large, orange ball."></img>
                 <figcaption className="talent__caption">Unboxing with Walter</figcaption>
+              </figure>
+            </a>
+          </div>
+
+          <div className="talent">
+            <a className="talent__link" href={talentLink4} target="_blank" rel="noopener noreferrer">
+              <figure 
+              className="talent__shape">
+
+                  <img src={breadAndCircus}
+                  className="talent__image"
+                  alt="A man on stilts in a nice, white tux standing behind a marching band in costume."></img>
+                <figcaption className="talent__caption">Bread and Circus!</figcaption>
               </figure>
             </a>
           </div>
